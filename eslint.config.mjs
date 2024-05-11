bc8be16964d29f046248fe63546d5b602b764471
加羲 <sunjiaxi.sjx@alibaba-inc.com>
2024-05-11 17:42:55 +0800

    chore: monorepo、eslint、prettier、husky

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import Prettier from "eslint-plugin-prettier/recommended";
+// const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  // eslintPluginPrettierRecommended
+  Prettier
+];
