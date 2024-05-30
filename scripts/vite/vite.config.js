6dd592626f06903c1580c11c6635cdc668bb4476
sjx.pc <87508191@qq.com>
2024-05-31 01:46:26 +0800

    chore: 使用vite实现实时调试

---
+++ scripts/vite/vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import replace from '@rollup/plugin-replace';
+import { resolvePkgPath } from '../rollup/utils';
+import path from 'path';
+
+// vite开发环境兼容rollup，生产环境完全rollup
+
+// vite的作用是实现实时更新包
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), replace({ __DEV__: true, preventAssignment: true })],
+  resolve: {
+    alias: [
+      {
+        find: 'react',
+        replacement: resolvePkgPath('react')
+      },
+      {
+        find: 'react-dom',
+        replacement: resolvePkgPath('react-dom')
+      },
+      {
+        find: 'hostConfig',
+        replacement: path.resolve(
+          resolvePkgPath('react-dom'),
+          './src/hostConfig.ts'
+        )
+      }
+    ]
+  }
+});
